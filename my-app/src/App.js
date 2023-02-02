bcdf130e64bc71bba7b37bdccf46854346f73fa5
clopez11224 <christopherlopez000@gmail.com>
2023-02-02 15:04:59 -0500

    Rendered a background image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing/landing.js"
+import image from "./images/zombie_wallpaper2.jpeg"
+
 
 function App() {
   return (
     <>
-    <div>Hello World</div>
-     
+      <div className="background-img" style={{ backgroundImage: `url(${image})` }}>
+        <Routes>
+          <Route path="/landing" element={<Landing />} />
+
+        </Routes>
+      </div>
+
     </>
   );
 }
---
+++ my-app/src/pages/landing/landing.js
@@ -0,0 +1,12 @@
+import React from "react"
+import image from "../../images/zombie_wallpaper2.jpeg"
+
+export default function Landing() {
+    // const { user } = useContext(AppContext)
+
+    return (
+        <div >
+        <div >This is the landing page</div>
+        </div>
+    )
+}
